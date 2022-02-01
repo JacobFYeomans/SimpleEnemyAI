19e9d99f391d73fc3b6a96be9715dc565fa6b773
PKMNRenunciation <yeomansjf@gmail.com>
2022-02-01 09:51:25 -0400

    added a timing system

--- Assets/Scripts/SimpleEnemyAI.cs
+++ Assets/Scripts/SimpleEnemyAI.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class SimpleEnemyAI : MonoBehaviour
 {
     public float moveSpeed = 0.5f;
-
+    private float timeSnapshot;
+    private float timeElapsed;
+    private int jumps;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeSnapshot = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        timeElapsed = Time.time - timeSnapshot;
+
+        if (timeElapsed > 5.0f)
+        {
+            Debug.Log("jump" + jumps);
+            jumps++;
+            timeElapsed = 0;
+            timeSnapshot = Time.time;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
